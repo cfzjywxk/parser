341473d80c39a5cabff74e1691f852eb12937c73
cfzjywxk <lsswxr@163.com>
2019-09-03 14:14:10 +0800

    add digest value into ast.Prepared struct

--- ast/misc.go
+++ ast/misc.go
@@ -321,6 +321,7 @@ type Prepared struct {
 	Params        []ParamMarkerExpr
 	SchemaVersion int64
 	UseCache      bool
+	DigestVal     string
 }
 
 // ExecuteStmt is a statement to execute PreparedStmt.
