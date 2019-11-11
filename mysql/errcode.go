60f7daedf9ba2148fbed9d871006df4886740f5b
cfzjywxk <lsswxr@163.com>
2019-11-11 16:25:37 +0800

    error code for sysvar.go

--- mysql/errcode.go
+++ mysql/errcode.go
@@ -972,6 +972,8 @@ const (
 	ErrIndexStateCantNone         = 8044
 	ErrInvalidRecordKey           = 8045
 	ErrColumnStateCantNone        = 8046
+	ErrUnsupportedValueForVar     = 8047
+	ErrNotSupportedIsolationLevel = 8048
 
 	// TiKV/PD errors.
 	ErrPDServerTimeout    = 9001
